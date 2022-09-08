e6aafb32a3a0ef0737cc9a900bb16b3658935590
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-08 11:55:15 +0100

    js

--- data.js
+++ data.js
@@ -3,13 +3,29 @@ const characterData = {
         name: "Wizard",
         avatar: "images/wizard.png",
         health: 60,
-        diceCount: 3
+        diceCount: 3,
+        currentDiceScore: []
     },
-    monster: {
+    orc: {
         name: "Orc",
         avatar: "images/orc.png",
-        health: 10,
-        diceCount: 1
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.png",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
     }
 }
 
